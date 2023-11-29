c44dfeff0049bd325088249ecefaa1c48e599d99
Don Grote <don.grote@gmail.com>
2023-11-29 06:15:03 -0500

    Squashed commit of the following:
    commit d06473085d4b1debc5a54b544e815a568f1fde17
    Author: Don Grote <don.grote@gmail.com>
    Date:   Wed Nov 29 06:14:51 2023 -0500
    
        merge sort works
    
        It's likely using a ton of space because it isn't in place, but it's functioning
    
    commit 54844276bdec52d2f1d4508ec0b14bbf9d608405
    Merge: 6271454 d59f845
    Author: Don Grote <don.grote@gmail.com>
    Date:   Mon Nov 27 06:41:07 2023 -0500
    
        Merge branch 'merge-sort' of https://github.com/dongrote/rust-kata into merge-sort
    
    commit 6271454b9b3dd86211d62c7459c3c322c5679430
    Author: Don Grote <don.grote@gmail.com>
    Date:   Mon Nov 27 11:38:32 2023 +0000
    
        merge-sort wip
    
    commit 2ac268dfe7a87b54f9cad961923c16163bcb58a9
    Author: Don Grote <don.grote@gmail.com>
    Date:   Fri Nov 17 10:33:44 2023 -0500
    
        wip merge sort
    
    commit d59f8455063f5de8d3b04c381df48b01c0dfd937
    Author: Don Grote <don.grote@gmail.com>
    Date:   Fri Nov 17 10:33:44 2023 -0500
    
        wip merge sort

---
+++ merge-sort/src/lib.rs
@@ -0,0 +1,104 @@
+use std::collections::VecDeque;
+
+fn _merge_sort(a: &Vec<u64>) -> Vec<u64> {
+    /*
+        MERGE-SORT(A, p, r)
+        if p < r
+        then q = floor((p + r) / 2)
+            merge_sort(A, p, q)
+            merge_sort(A, q + 1, r)
+            merge(A, p, q, r)
+     */
+    if a.len() > 1 {
+        let q = a.len() / 2;
+        let left = a[..q].to_vec();
+        let right = a[q..].to_vec();
+        let mut sorted_left = VecDeque::from(_merge_sort(&left));
+        let mut sorted_right = VecDeque::from(_merge_sort(&right));
+        _merge(&mut sorted_left, &mut sorted_right)
+    } else {
+        a.clone()
+    }
+}
+
+fn _merge(l: &mut VecDeque<u64>, r: &mut VecDeque<u64>) -> Vec<u64> {
+    /*
+        MERGE(A, p, q, r)
+        n1 = q - p + 1
+        n2 = r - q
+        let mut L = vec![0..(n1 + 1)]
+        let mut R = vec![0..(n2 + 1)]
+        for i in 0..n1
+            L[i] = A[p + i - 1]
+        for j in 0..n2
+            R[i] = A[q + j]
+        i = j = 0
+        for k in p..r
+            loop {
+                if L[i] <= R[j] {
+                    A[k] = L[i]
+                    i++
+                } else {
+                    A[k] = R[j]
+                    j++
+                }
+            }
+     */
+    let mut return_vec: Vec<u64> = Vec::new();
+    let mut next_left = l.pop_front();
+    let mut next_right = r.pop_front();
+    loop {
+        match next_left {
+            Some(left) => match next_right {
+                Some(right) => {
+                    if left <= right {
+                        return_vec.push(left);
+                        next_left = l.pop_front();
+                    } else {
+                        return_vec.push(right);
+                        next_right = r.pop_front();
+                    }
+                },
+                None => {
+                    return_vec.push(left);
+                    next_left = l.pop_front();
+                },
+            },
+            None => match next_right {
+                Some(right) => {
+                    return_vec.push(right);
+                    next_right = r.pop_front();
+                },
+                None => break,
+            }
+        }
+    }
+
+    return_vec
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge_sort_empty_array() {
+        let a: Vec<u64> = vec![];
+        let sorted = _merge_sort(&a);
+        assert_eq!(sorted, []);
+    }
+
+    #[test]
+    fn test_merge_sort_even_sized_array() {
+        let a: Vec<u64> = vec![54, 6, 3, 6, 62, 5, 252, 62, 42, 54];
+        let sorted = _merge_sort(&a);
+        assert_eq!(sorted, vec![3, 5, 6, 6, 42, 54, 54, 62, 62, 252]);
+    }
+
+    #[test]
+    fn test_merge_sort_odd_sized_array() {
+        let a: Vec<u64> = vec![54, 6, 3, 6, 62, 5, 252, 62, 42];
+        let sorted = _merge_sort(&a);
+        assert_eq!(sorted, vec![3, 5, 6, 6, 42, 54, 62, 62, 252]);
+    }
+}
